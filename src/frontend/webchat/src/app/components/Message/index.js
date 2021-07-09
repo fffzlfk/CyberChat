ca70b33eae093db8a65396b6ff33125c47355fba
ffflzfk <fffzlfk@DESKTOP-U99TE3D.localdomain>
2021-07-09 21:06:47 +0800

    updare-struct

--- src/frontend/webchat/src/app/components/Message/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export class Message extends React.Component {
-    render() {
-        const { messages } = this.props;
-
-        return messages && messages.map(msg => <p key={msg.ID}><strong>{msg.sender + ": "}</strong>{msg.body} <small>{msg.time}</small></p>)
-    }
-}
